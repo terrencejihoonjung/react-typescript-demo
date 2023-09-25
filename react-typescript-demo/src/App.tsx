1e77eb5d92fadac61e87585e3d1928f93ad1085c
terrencejihoonjung <terrencejung928@gmail.com>
2023-09-25 13:00:11 -0700

    typing props

---
+++ react-typescript-demo/src/App.tsx
@@ -0,0 +1,11 @@
+import Greet from "./components/Greet";
+
+function App() {
+  return (
+    <div>
+      <Greet name="Terrence" />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-typescript-demo/src/components/Greet.tsx
@@ -0,0 +1,14 @@
+// Specify the types for each property in the props
+type GreetProps = {
+  name: string;
+};
+
+function Greet({ name }: GreetProps) {
+  return (
+    <div>
+      <h2>Welcome {name}! You have 10 unread messages</h2>
+    </div>
+  );
+}
+
+export default Greet;
